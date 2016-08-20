f12508e5070e8777c1c63ea8bf7944e6fa5bd42a
sahaya.thanislas <georgetsahaya@gmail.com>
2016-08-20 19:06:24 +1000

    Initial commit with skeleton

---
+++ app/src/main/java/app/weather/com/weatherapp/WeatherActivity.java
@@ -0,0 +1,31 @@
+package app.weather.com.weatherapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WeatherActivity extends AppCompatActivity {
+    public static final String API_KEY = "f3cc1b22c0232ac17cdaaa7dced8322e";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
